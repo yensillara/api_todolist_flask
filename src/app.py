f8f32e9eceddf0170d9e526bf23302b05bcd7a98
yensillara <arquiperiodista@gmail.com>
2021-01-22 00:25:53 +0000

    proyecto terminado

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request, json
+app = Flask(__name__)
+
+todos = [
+    {"label": "My first task", "done": False}
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads (request_body)
+    todos.append (decoded_object)
+    return jsonify(todos)
+
+@app.route ('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    return jsonify(todos)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
